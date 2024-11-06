61bca74996564af37d49d6ce8a28006744762c88
Rahul Kumar Yadav <ry573870@gmail.com>
2024-11-06 17:37:06 +0530

    add test file

---
+++ Basics/test.php
@@ -0,0 +1,15 @@
+<?php
+$name = "Rahul yadav";
+$age = 22;
+echo strtoupper($name);
+
+
+echo str_replace("Rahul","Anshu", $name);
+
+
+ECHO "Rahul is a good boy";
+echo "<h1>Hello world </h1>"; 
+echo"Hi my name is $name <br>";
+echo"My age is $age years old<br>";
+ECHO "Hello Rahul";
+?>
