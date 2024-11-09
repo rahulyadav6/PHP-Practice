21d2cd3c6a0a9d65212d3019680adcaf9204a644
Rahul Kumar Yadav <ry573870@gmail.com>
2024-11-09 16:23:59 +0530

    php include complete

---
+++ Basics/includePHP/site.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $title = "My First Post";
+        $author = "Rahul";
+        $wordCount = 537;
+        include "articleHeader.php";
+        include "usefull-tools.php";
+        sayHi("Rahul");
+    ?>
+</body>
+</html>
