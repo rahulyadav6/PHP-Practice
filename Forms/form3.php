12efbbddd23c1ad6b3b7f8df21301c3d15944162
Rahul Kumar Yadav <ry573870@gmail.com>
2024-11-18 16:58:41 +0530

    Forms in php

---
+++ Forms/form3.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+<form action="form.php" method="get">
+    Color: <input type="text" name ="color"><br>
+    Plural Noun: <input type="text" name="pluralNoun"><br>
+    Celebrity: <input type="text" name = "celebrity"><br>
+    <input type="submit" value="Submit">
+</form>
+<?php 
+    if(isset($_GET["color"])) $color = $_REQUEST["color"];
+    if(isset($_GET["pluralNoun"])) $pluralNoun = $_REQUEST["pluralNoun"];
+    if(isset($_GET["celebrity"])) $celebrity = $_REQUEST["celebrity"];
+    if(isset($color) && isset($pluralNoun) && isset($celebrity)){
+        echo "Roses are $color <br>";
+        echo "$pluralNoun are blue <br>";
+        echo "I love $celebrity<br>";
+    }
+?>
+</body>
+</html>
---
+++ Forms/formValidation.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>PHP Form Validation Example</h2>
+    <form action="formValidation.php" method="post" >
+        Name: <input type="text" name="name"><br><br>
+        E-mail: <input type="text" name="email"><br><br>
+        Website: <input type="text" name="website"><br><br>
+        Comment: <textarea name="comment" rows="5" cols="40"></textarea><br><br>
+        <input type="radio" name="gender" value="female">Female
+        <input type="radio" name="gender" value="male">Male
+        <input type="radio" name="gender" value="other">Other
+    </form>
+    <?php
+        
+    ?>
+</body>
+</html>
